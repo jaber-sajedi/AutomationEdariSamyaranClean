d34525f17407419132902b10f08b8a36ed419f3f
jaber sajedi <jaber.sajedi@gmail.com>
2025-09-28 18:39:41 +0330

    add Filters/CustomExceptionFilter.cs

---
+++ AutomationEdariSamyaran/AutomationEdariSamyaran.API/LoginModelExampleFilter.cs
@@ -0,0 +1,22 @@
+﻿using AutomationEdariSamyaran.Domain.Entities;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace AutomationEdariSamyaran.API
+{
+    public class LoginModelExampleFilter : ISchemaFilter
+    {
+        public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+        {
+            if (context.Type == typeof(LoginModel))
+            {
+                schema.Example = new OpenApiObject
+                {
+                    ["username"] = new OpenApiString("admin"),
+                    ["password"] = new OpenApiString("1234")
+                };
+            }
+        }
+    }
+}
