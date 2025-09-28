2429220c958644f04f0748fb9cc5b8dcead7f611
jaber sajedi <jaber.sajedi@gmail.com>
2025-09-28 18:40:11 +0330

    add

---
+++ AutomationEdariSamyaran/AutomationEdariSamyaran.Application/Behaviors/ExceptionHandlingBehavior.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationEdariSamyaran.Application.Behaviors
+{
+    public class ExceptionHandlingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        private readonly ILogger<ExceptionHandlingBehavior<TRequest, TResponse>> _logger;
+
+        public ExceptionHandlingBehavior(ILogger<ExceptionHandlingBehavior<TRequest, TResponse>> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            try
+            {
+                return await next();
+            }
+            catch (Exception ex)
+            {
+                var requestName = typeof(TRequest).Name;
+                _logger.LogError(ex, "An error occurred while handling request {RequestName}", requestName);
+                throw;
+            }
+
+        }
+    }
+
+}
---
+++ AutomationEdariSamyaran/AutomationEdariSamyaran.Application/Common/Hash_Class.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationEdariSamyaran.Application.Common
+{
+    public class Hash_Class
+    {
+        // تولید MD5
+        public static string GetMd5Hash(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                return string.Empty;
+
+            using (MD5 md5Hasher = MD5.Create())
+            {
+                byte[] data = md5Hasher.ComputeHash(Encoding.UTF8.GetBytes(input.Trim()));
+
+                StringBuilder sBuilder = new StringBuilder();
+                foreach (byte b in data)
+                    sBuilder.Append(b.ToString("x2")); // همیشه حروف کوچک
+
+                return sBuilder.ToString();
+            }
+        }
+
+        // بررسی برابر بودن هش
+        public static bool VerifyMd5Hash(string input, string hash)
+        {
+            if (string.IsNullOrWhiteSpace(input) || string.IsNullOrWhiteSpace(hash))
+                return false;
+
+            string hashOfInput = GetMd5Hash(input).Trim().ToLower();
+            hash = hash.Trim().ToLower();
+
+            return hashOfInput.Equals(hash, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
