e93a49e1506bc5d75b6181d6d1adb92b589403af
jaber sajedi <jaber.sajedi@gmail.com>
2025-10-12 18:43:51 +0330

    Workflow

---
+++ AutomationEdariSamyaran/AutomationEdariSamyaran.Domain/Entities/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
+
+namespace AutomationEdariSamyaran.Domain.Entities
+{
+    public class Department
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public int SectionId { get; set; }
+      //  public Section Section { get; set; } = null!;
+       // public ICollection<Employee> Employees { get; set; } = new List<Employee>();
+    }
+}
---
+++ AutomationEdariSamyaran/AutomationEdariSamyaran.Domain/Entities/Position.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationEdariSamyaran.Domain.Entities
+{
+    public class Position
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = null!; // نام سمت شغلی
+
+        [MaxLength(200)]
+        public string? Description { get; set; } // توضیح سمت
+    }
+}
